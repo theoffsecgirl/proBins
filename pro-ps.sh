2cee1d4e1e6496102716f1ba3205b3c0e028c7c4
Arianne (gh0stfvce) <114576266+gh0stfvvce@users.noreply.github.com>
2023-12-03 18:13:50 +0100

    Create pro-ps.sh

---
+++ pro-ps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ps -eo pid,comm,%cpu,%mem
+# Mejora la salida de ps para mostrar solo las columnas más relevantes, como el ID del proceso, el nombre del comando, el uso de CPU y memoria.
