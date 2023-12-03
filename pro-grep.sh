29f7056282813c969de9e9950369080412c40807
Arianne (gh0stfvce) <114576266+gh0stfvvce@users.noreply.github.com>
2023-12-03 18:21:44 +0100

    Create pro-grep.sh

---
+++ pro-grep.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+grep -rn --color=auto #"patron" .
+#Un script que realiza búsquedas en archivos de manera más avanzada, con opciones como la coincidencia de patrones y la visualización de líneas circundantes.
