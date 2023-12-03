834d3cf24df5740a87540ee264acbfb4c94e8eba
Arianne (gh0stfvce) <114576266+gh0stfvvce@users.noreply.github.com>
2023-12-03 18:16:58 +0100

    Create pro-ls.sh

---
+++ pro-ls.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ls -ltrah --color=auto --group-directories-first
+
+# En esta versión más detallada;
+# color=auto habilita el resaltado de colores según el tipo de archivo.
+# group-directories-first agrupa los directorios primero en la lista.
